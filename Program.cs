8bda212cc5154f691dd03c15b7a97c4c9105e4c3
pradadanhafiidh <pradanahafidh@gmail.com>
2020-05-12 20:45:03 +0700

    Tugas 6 Polymorphism Interface

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           atas:
+            IPrinterWindows printer;
+            Console.WriteLine("Pilihan Printer :");
+            Console.WriteLine("1.Epson");
+            Console.WriteLine("2.Canon");
+            Console.WriteLine("3.LaserJet");
+            Console.Write("Nomor printer [1,2,3] : ");
+            int pilihanPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if(pilihanPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (pilihanPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.show();
+            printer.print();
+
+            Console.ReadKey();
+
+            goto atas;
+        }
+    }
+}
